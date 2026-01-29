6103123e76eb66c61617bd50a5bc2b4fda0717a5
PratypartyY2K <pkk5421@psu.edu>
2026-01-29 14:19:49 -0500

    Add scripts for training BPE tokenizer on sample datasets

---
+++ assignment1-basics/scripts/train_owt_small.py
@@ -0,0 +1,25 @@
+import time, pickle
+from cs336_basics.tokenizer import train_bpe
+
+input_path = 'data/owt_train_sample_1mb.txt'
+vocab_size = 32000
+special_tokens = ['<|endoftext|>']
+
+start = time.time()
+vocab, merges = train_bpe(input_path, vocab_size, special_tokens)
+end = time.time()
+
+with open('owt_small_tokenizer_vocab.pkl','wb') as f:
+    pickle.dump(vocab,f)
+with open('owt_small_tokenizer_merges.pkl','wb') as f:
+    pickle.dump(merges,f)
+
+longest_token_id = max(vocab.keys(), key=lambda k: len(vocab[k]))
+longest_token = vocab[longest_token_id]
+
+print('elapsed_seconds=', end-start)
+print('vocab_size=', len(vocab))
+print('num_merges=', len(merges))
+print('longest_token_id=', longest_token_id)
+print('longest_token_bytes_len=', len(longest_token))
+print('longest_token_utf8=', longest_token.decode('utf-8',errors='replace'))
